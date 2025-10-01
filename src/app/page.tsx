2dba06c3cbef5640b985d7d455212b37130d97a6
Saswat Barai <saswatbarai611@gmail.com>
2025-10-01 22:41:11 +0530

    Implement user authentication with social login and session management
    - Added User, Session, Account, and Verification models to Prisma schema.
    - Created migration for authentication tables in the database.
    - Developed authentication layout and sign-in page with GitHub and Google login options.
    - Implemented API routes for authentication handling.
    - Created client-side authentication utilities for session management.
    - Added user button component for displaying user info and handling logout.
    - Integrated environment variables for social provider credentials.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from '@/components/ui/button'
+import { currentUser } from '@/modules/authentication/actions'
+import UserButton from '@/modules/authentication/components/user-button';
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
+  const user = await currentUser();
   return (
     <div>
-      <Button>Hello</Button>
+      <div className='flex h-screen flex-col items-center justify-center'>
+       <UserButton user={user}/>
+      </div>
     </div>
   )
 }
